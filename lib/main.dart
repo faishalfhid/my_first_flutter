0447e1586f42bdfdbf040ab8277dbe2fa06f6101
faishalfhid <faishalfh1.work@gmail.com>
2026-01-01 10:55:47 +0700

    Perbedaan column, row, stack

--- lib/main.dart
+++ lib/main.dart
@@ -15,19 +15,30 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blue,
         ),
         backgroundColor: Colors.white,
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              print("HALLO");
-            },
-            child: Icon(Icons.thumb_up, size: 24, color: Colors.blue),
-          ),
-          // Container(
-          //   width: 200,
-          //   height: 50,
-          //   color: Colors.amberAccent,
-          //   child: FlutterLogo(size: 200),
-          // ),
+        // Column untuk arah vertikal
+        // Row untuk arah horizontal
+        // Stack menumpuk
+        body: Stack(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.amber,
+              child: Center(child: Text("Container 1")),
+            ),
+            Container(
+              width: 150,
+              height: 150,
+              color: Colors.green,
+              child: Center(child: Text("Container 2")),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              child: Center(child: Text("Container 3")),
+            ),
+          ],
         ),
       ),
     );
