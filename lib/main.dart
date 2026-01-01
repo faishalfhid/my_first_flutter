cbf144f61a9122597d4f3b8ea60f2c27fb9cd71c
faishalfhid <faishalfh1.work@gmail.com>
2026-01-01 17:42:32 +0700

    Extract Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,42 @@
 import 'package:flutter/material.dart';
+import 'widgets/KotakWarna.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Aplikasi Pertamaku"),
-          centerTitle: true,
+          title: Center(child: Text("Aplikasi Pertamaku")),
           backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
         ),
-        backgroundColor: Colors.white,
-        // Column untuk arah vertikal
-        // Row untuk arah horizontal
-        // Stack menumpuk
-        // SingleChildScroolView -> Agar bisa discroll
-        // listview -> gabungan antara column, row, dan singlechildscroolview
-        // MainAxisAlignment -> mengatur alignment layout: start, center, end, spacearround, spacebetween
-        // CrossAxisAlignment -> kebalikan dari main (90 derajat dari main axis)
-        // GridView -> mengatur layout seperti grid instagram
-        // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount -> Membagi rata grid dengan tinggi dan lebar sama rata berdasarkan angka count
-        // gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent -> Membagi rata grid berdasarkan ukuran maximum
-        // mainAxisSpacing & crossAxisSpacing -> seperti padding tapi berdasarkan main/cross axis
-        // padding: EdgeInsets.only -> mengatur padding satu2
-        // padding: EdgeInsets.all -> mengatur padding langsung semua
-        body: GridView(
-          padding: EdgeInsets.only(top: 10, left: 10, right: 10, bottom: 10),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            childAspectRatio: 1 / 2,
-          ),
+        body: Column(
           children: [
-            Container(color: Colors.red),
-            Container(color: Colors.yellow),
-            Container(color: Colors.green),
-            Container(color: Colors.blue),
+            KotakWarna(text: "merah", warna: Colors.red),
+            KotakWarna(text: "hijau", warna: Colors.green),
+            KotakWarna(text: "blue", warna: Colors.blue),
+            KotakWarna(text: "merah", warna: Colors.red),
+            KotakWarna(text: "hijau", warna: Colors.green),
+            KotakWarna(text: "blue", warna: Colors.blue),
+            KotakWarna(text: "merah", warna: Colors.red),
+            KotakWarna(text: "hijau", warna: Colors.green),
+            KotakWarna(text: "blue", warna: Colors.blue),
+            KotakWarna(text: "merah", warna: Colors.red),
+            KotakWarna(text: "hijau", warna: Colors.green),
+            KotakWarna(text: "blue", warna: Colors.blue),
+            KotakWarna(text: "merah", warna: Colors.red),
+            KotakWarna(text: "hijau", warna: Colors.green),
+            KotakWarna(text: "blue", warna: Colors.blue),
+            KotakWarna(text: "merah", warna: Colors.red),
+            KotakWarna(text: "hijau", warna: Colors.green),
+            KotakWarna(text: "blue", warna: Colors.blue),
           ],
         ),
       ),
